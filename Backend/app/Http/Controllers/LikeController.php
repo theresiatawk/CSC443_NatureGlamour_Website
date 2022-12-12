64d5de7258892a0ec14691e577370c91e24a2015
Theresia Tawk <theresiatawk@hotmail.com>
2022-12-12 15:36:14 +0200

    Backend: Implemnting delete like api

--- Backend/app/Http/Controllers/LikeController.php
+++ Backend/app/Http/Controllers/LikeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Like;
+use Validator;
 
 
 
@@ -25,27 +26,29 @@ class LikeController extends Controller
         //Check if user and post exists
         $user = User::find($request->user_id);
         $post = Post::find($request->post_id);
-        if(!user){
+        if(!$user){
             return response()->json([
                 "status" => "error",
                 "results" => "Invalid User"
             ], 401);
         }
-        if(!post){
+        if(!$post){
             return response()->json([
                 "status" => "error",
                 "results" => "Invalid Post"
             ], 401);
         }
+        //Check if user already like this post
         $like = Like::where("post_id",$request->post_id)
                     ->where("user_id",$request->user_id)
                     ->get();
         if(count($like) > 0){
             return response()->json([
                 "status" => "error",
-                "results" => "Post already liked by this user"
+                "results" => "User already like this post"
             ], 401);
         }
+        // Adding new like
         $like = new Like;
         $like->user_id = $request->user_id;
         $like->post_id = $request->post_id;
@@ -55,6 +58,28 @@ class LikeController extends Controller
                 'results' => 'Like Added',
                 'post' => $post
             ], 200);
+        }
+    }
+    function deleteLike(){
+        $validate = Validator::make($request->all(), [
+            'user_id' => 'required|integer',
+            'post_id' => 'required|integer'
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                "status" => "error",
+                "results" => "Some fields are empty"
+            ], 400);
+        }
+        //Check if like exist
+        $like = Like::where("post_id",$request->post_id)
+                    ->where("user_id",$request->user_id)
+                    ->get();
+        if(count($like) == 0){
+            return response()->json([
+                "status" => "error",
+                "results" => "Like does not exist"
+            ], 404);
         }
     }
 }
