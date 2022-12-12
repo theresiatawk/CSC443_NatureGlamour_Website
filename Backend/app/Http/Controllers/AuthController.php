2c30212be9ed8aa0cc2556f1e23c733db1add8c0
Theresia Tawk <theresiatawk@hotmail.com>
2022-12-12 09:07:37 +0200

    Backend: Coorecting signup api

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -3,32 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+    function register(Request $request){
+        $validate = Validator::make($request->all(), [
+            'username' => 'required|string|alpha_dash|max:255',
+            'email' => 'required|string|regex:/(.+)@(.+)\.(.+)/i|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
-        $user = User::create([
-            'username' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        if($validate->fails()){
+            return response()->json([
+                "status" => "failed",
+                "results" => []
+            ], 400);
+        }
+        $user = new User;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
 
-        $token = Auth::login($user);
-        return response()->json([
-            'status' => 'success',
-            'message' => 'User created successfully',
-            'user' => $user,
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
-        ]);
+        if($user->save()){
+            return response()->json([
+                "status" => "success",
+                "results" => $user
+            ], 200);
+        }else{
+            return response()->json([
+                "status" => "failure",
+                "results" => []
+            ], 400);
+        }
     }
 }
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\AuthController;
 // });
 Route::group(["prefix"=> "v0.1"], function(){
     Route::controller(AuthController::class)->group(function () {
-        Route::post("/signin", 'login');
-        // Route::post("/signup", 'register');
+        // Route::post("/signin", 'login');
+        Route::post("/signup", 'register');
     });
 });
