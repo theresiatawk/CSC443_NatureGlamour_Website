0b6ea887243e90b2b3590191abdb5b290e8f44d5
Theresia Tawk <theresiatawk@hotmail.com>
2022-12-13 03:19:26 +0200

    Frontend: Setting up for linking

--- Frontend/Javascripts/page_script.js
+++ Frontend/Javascripts/page_script.js
@@ -16,3 +16,22 @@ nature_glamour_pages.Console = (title, values, oneValue = true) => {
 nature_glamour_pages.loadFor = (page) => {
   eval("nature_glamour_pages.load_" + page + "();");
 };
+instagram_like_pages.postAPI = async (api_url, api_data, api_token = null) => {
+  try {
+    return await axios.post(api_url, api_data, {
+      headers: {
+        Authorization: "token " + api_token,
+        // "Content-Type": "multipart/form-data boundary=something",
+      },
+    });
+  } catch (error) {
+    instagram_like_pages.Console("Error from Linking (POST)", error);
+  }
+};
+instagram_like_pages.getAPI = async (api_url) => {
+  try {
+    return await axios(api_url);
+  } catch (error) {
+    instagram_like_pages.Console("Error from Linking (GET)", error);
+  }
+};
