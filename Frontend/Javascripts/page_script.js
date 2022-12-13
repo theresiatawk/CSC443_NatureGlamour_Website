ba81df2fdaef97243ceb31764514adeefa5dd2f5
Theresia Tawk <theresiatawk@hotmail.com>
2022-12-13 03:38:08 +0200

    Frontend: Implemnting the signup linking

--- Frontend/Javascripts/page_script.js
+++ Frontend/Javascripts/page_script.js
@@ -16,7 +16,7 @@ nature_glamour_pages.Console = (title, values, oneValue = true) => {
 nature_glamour_pages.loadFor = (page) => {
   eval("nature_glamour_pages.load_" + page + "();");
 };
-instagram_like_pages.postAPI = async (api_url, api_data, api_token = null) => {
+nature_glamour_pages.postAPI = async (api_url, api_data, api_token = null) => {
   try {
     return await axios.post(api_url, api_data, {
       headers: {
@@ -25,13 +25,32 @@ instagram_like_pages.postAPI = async (api_url, api_data, api_token = null) => {
       },
     });
   } catch (error) {
-    instagram_like_pages.Console("Error from Linking (POST)", error);
+    nature_glamour_pages.Console("Error from Linking (POST)", error);
   }
 };
-instagram_like_pages.getAPI = async (api_url) => {
+nature_glamour_pages.getAPI = async (api_url) => {
   try {
     return await axios(api_url);
   } catch (error) {
-    instagram_like_pages.Console("Error from Linking (GET)", error);
+    nature_glamour_pages.Console("Error from Linking (GET)", error);
   }
 };
+nature_glamour_pages.load_register = () => {
+    const signup_btn = document.getElementById("register");
+    const result = document.getElementById("response");
+  
+    const signup = async () => {
+      const signup_url = base_url + "signup";
+  
+      const signup_data = new URLSearchParams();
+      signup_data.append("useranme", document.getElementById("username").value);
+      signup_data.append("email", document.getElementById("email").value);
+      signup_data.append("password", document.getElementById("password").value);
+  
+      const response = await nature_glamour_pages.postAPI(
+        signup_url,
+        signup_data
+      );
+      console.log(response);
+    };
+}
